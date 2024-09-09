542adc79f073b372e4682c8146222af143fc7cd8
sandeep <snpoudelskt@gmail.com>
2024-09-09 16:05:04 -0400

    renamed

---
+++ 3.6submit_regional_lstm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=regional-lstm            # Job name
+#SBATCH --output=regional-lstm%j.log       # Output file name (%j expands to jobID)
+#SBATCH --error=regional-lstm%j.log        # Error file name (%j expands to jobID)
+#SBATCH --time=32:00:00                 # Time limit (HH:MM:SS)
+#SBATCH --nodes=5                       #5 Number of nodes
+#SBATCH --ntasks=5                    #394 Number of tasks (one for each job), if you don't know numner of tasks beforehand there are ways to make this input dynamic as well
+#SBATCH --cpus-per-task=16               # Number of CPU cores per task
+#SBATCH --mem=164G                        # Memory per CPU core (adjust as needed)
+#SBATCH --exclusive                     # Exclusive node allocation
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sp2596@cornell.edu
+
+# Load necessary modules
+# All modules are loaded inside the virtual environment so don't need to load here (check: pip list modules when virtual environment is loaded) 
+module load python/3.11.5
+# Activate your virtual environment if needed
+source ~/pyenv-pytorch/bin/activate
+
+# Run your Python script with mpi
+mpirun python3 3.7regional_lstm_idw_streamflow.py
