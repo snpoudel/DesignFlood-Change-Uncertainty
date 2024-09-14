f806ec9b6f5d86c01887427b4afb885bede63d92
sandeep <snpoudelskt@gmail.com>
2024-09-14 14:00:54 -0400

    scripts created to make precip and flow qq plots

---
+++ 10.5eval_everything_qq.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+#write a function to calculate RMSE
+def rmse(q_obs, q_sim):
+    rmse_value = np.sqrt(np.mean((q_obs - q_sim)**2))
+    return rmse_value
+
+
+#read list of basins
+basin_list = pd.read_csv('data/MA_basins_gauges_2000-2020_filtered.csv',dtype={'basin_id':str})
+#basin grid and combination of interest
+id = '01108000'
+
+#precip
+true_precip = pd.read_csv(f'data/true_precip/true_precip{id}.csv')
+future_true_precip = pd.read_csv(f'data/future/future_true_precip/future_true_precip{id}.csv')
+#flow
+true_flow = pd.read_csv(f'output/hbv_true_streamflow/hbv_true_output_{id}.csv')
+future_true_flow = pd.read_csv(f'output/future/hbv_true_future_streamflow/hbv_true_future_output_{id}.csv')
+
+#function to read precip error and categorize into one of these buckets
+def precip_bucket(precip_error):
+    if precip_error <=2:
+        return('0-2')
+    elif precip_error <=4:
+        return('2-4')
+    elif precip_error <=6:
+        return('4-6')
+    elif precip_error <=8:
+        return('6-8')
+    elif precip_error <=10:
+        return('8-10')
+    else:
+        return('>10')
+    
+
+
+df = pd.DataFrame()#dataframe store historical precip data
+dff =pd.DataFrame()#dataframe store future precip data
+
+fd = pd.DataFrame()#dataframe store historical flow data
+ffd =pd.DataFrame()#dataframe store future flow data
+
+#read interpolated precipitaiton
+for coverage in np.arange(30):
+    for comb in np.arange(15):
+        file_path = f'data/idw_precip/idw_precip{id}_coverage{coverage}_comb{comb}.csv'
+        if os.path.exists(file_path):
+            idw_precip = pd.read_csv(file_path)
+            future_idw_precip=pd.read_csv(f'data/future/future_idw_precip/future_idw_precip{id}_coverage{coverage}_comb{comb}.csv')
+            precip_rmse = rmse(true_precip['PRECIP'], idw_precip['PRECIP'])
+            pb = precip_bucket(precip_rmse) #categorize into a precip bucket
+            idw_precip['tag'] = pb
+            idw_precip['truth'] = true_precip['PRECIP']
+            future_idw_precip['tag'] = pb
+            future_idw_precip['truth'] = future_true_precip['PRECIP']
+            df = pd.concat([df, idw_precip])
+            dff = pd.concat([dff, future_idw_precip])
+
+            #Read corresponding streamflow for HBV recalibrated model
+            hbvre = pd.read_csv(f'output/hbv_idw_recalib_streamflow/hbv_idw_recalib_streamflow{id}_coverage{coverage}_comb{comb}.csv')
+            future_hbvre = pd.read_csv(f'output/future/hbv_idw_recalib_future_streamflow/hbv_idw_recalib_future_streamflow{id}_coverage{coverage}_comb{comb}.csv')
+            hbvre['tag'] = pb
+            hbvre['truth'] = true_flow['streamflow']
+            hbvre['model'] = 'HBV Re'
+            future_hbvre['tag'] =pb
+            future_hbvre['truth'] = future_true_flow['streamflow']
+            future_hbvre['model'] = 'HBV Re'
+            fd = pd.concat([fd, hbvre])
+            ffd  = pd.concat([ffd, future_hbvre])
+
+            #Read corresponding streamflow for hymod  model
+            hymod = pd.read_csv(f'output/hymod_idw_streamflow/hymod_interpol_streamflow{id}_coverage{coverage}_comb{comb}.csv')
+            future_hymod = pd.read_csv(f'output/future/hymod_idw_future_streamflow/hymod_interpol_future_streamflow{id}_coverage{coverage}_comb{comb}.csv')
+            hymod['tag'] = pb
+            hymod['truth'] = true_flow['streamflow']
+            hymod['model'] = 'HYMOD'
+            future_hymod['tag'] =pb
+            future_hymod['truth'] = future_true_flow['streamflow']
+            future_hymod['model'] = 'HYMOD'
+            fd = pd.concat([fd, hymod])
+            ffd  = pd.concat([ffd, future_hymod])
+
+            #Read corresponding streamflow for lstm  model
+            lstm = pd.read_csv(f'output/lstm_idw_streamflow/lstm_idw_{id}_coverage{coverage}_comb{comb}.csv')
+            future_lstm = pd.read_csv(f'output/future/lstm_idw_future_streamflow/lstm_idw_future_streamflow{id}_coverage{coverage}_comb{comb}.csv')
+            lstm['tag'] = pb
+            lstm['truth'] = true_flow['streamflow']
+            lstm['model'] = 'LSTM'
+            future_lstm['tag'] =pb
+            future_lstm['truth'] = future_true_flow['streamflow']
+            future_lstm['model'] = 'LSTM'
+            fd = pd.concat([fd, lstm])
+            ffd  = pd.concat([ffd, future_lstm])
+
+
+#save the plots
+df.to_csv('output/hist_precip_by_buckets.csv', index=False)
+dff.to_csv('output/future_precip_by_buckets.csv', index=False)
+fd.to_csv('output/hist_flow_by_buckets.csv', index=False)
+ffd.to_csv('output/future_flow_by_buckets.csv', index=False)
---
+++ 10.6eval_everything_qqplot.py
@@ -0,0 +1,313 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#color palette
+palette = sns.color_palette('colorblind')
+#read truth
+id = '01108000'
+#read precip
+hist_precip = pd.read_csv('output/hist_precip_by_buckets.csv')
+future_precip = pd.read_csv('output/future_precip_by_buckets.csv')
+
+#read flow
+hist_flow = pd.read_csv('output/hist_flow_by_buckets.csv')
+future_flow = pd.read_csv('output/future_flow_by_buckets.csv')
+
+
+##--HISTORICAL--##
+fig,axs=plt.subplots(4,4, figsize=(9,8))
+#precip
+axs[0][0].scatter(data=hist_precip[hist_precip['tag']=='0-2'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][0].set_title('MAP RMSE: 0-2')
+axs[0][0].set_ylabel('Interpolated MAP')
+axs[0][0].set_xlim(-10,180)
+axs[0][0].set_ylim(-10,180)
+axs[0][0].set_aspect('equal')
+axs[0][0].grid(True, linestyle='--',alpha = 0.5)
+
+axs[0][1].scatter(data=hist_precip[hist_precip['tag']=='2-4'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][1].set_title('MAP RMSE: 2-4')
+axs[0][1].set_xlim(-10,180)
+axs[0][1].set_ylim(-10,180)
+axs[0][1].set_aspect('equal')
+axs[0][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[0][2].scatter(data=hist_precip[hist_precip['tag']=='4-6'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][2].set_title('MAP RMSE: 4-6')
+axs[0][2].set_xlim(-10,180)
+axs[0][2].set_ylim(-10,180)
+axs[0][2].set_aspect('equal')
+axs[0][2].grid(True, linestyle='--',alpha = 0.5)
+
+
+axs[0][3].scatter(data=hist_precip[hist_precip['tag']=='6-8'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][3].set_title('MAP RMSE: 6-8')
+axs[0][3].set_aspect('equal')
+axs[0][3].set_xlim(-10,180)
+axs[0][3].set_ylim(-10,180)
+axs[0][3].set_aspect('equal')
+axs[0][3].grid(True, linestyle='--',alpha = 0.5)
+
+
+#hbv recalibrated
+axs[1][0].scatter(data=hist_flow[(hist_flow['model']=='HBV Re') & (hist_flow['tag']=='0-2')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][0].set_ylabel('Simulated HBV-Re flow')
+axs[1][0].set_xlim(-1.2,25)
+axs[1][0].set_ylim(-1.2,25)
+axs[1][0].set_aspect('equal')
+axs[1][0].grid(True, linestyle='--',alpha = 0.5)
+
+
+axs[1][1].scatter(data=hist_flow[(hist_flow['model']=='HBV Re') & (hist_flow['tag']=='2-4')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][1].set_xlim(-1.2,25)
+axs[1][1].set_ylim(-1.2,25)
+axs[1][1].set_aspect('equal')
+axs[1][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[1][2].scatter(data=hist_flow[(hist_flow['model']=='HBV Re') & (hist_flow['tag']=='4-6')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][2].set_xlim(-1.2,25)
+axs[1][2].set_ylim(-1.2,25)
+axs[1][2].set_aspect('equal')
+axs[1][2].grid(True, linestyle='--',alpha = 0.5)
+
+axs[1][3].scatter(data=hist_flow[(hist_flow['model']=='HBV Re') & (hist_flow['tag']=='6-8')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][3].set_xlim(-1.2,25)
+axs[1][3].set_ylim(-1.2,25)
+axs[1][3].set_aspect('equal')
+axs[1][3].grid(True, linestyle='--',alpha = 0.5)
+
+#HYMOD
+axs[2][0].scatter(data=hist_flow[(hist_flow['model']=='HYMOD') & (hist_flow['tag']=='0-2')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][0].set_ylabel('Simulated HYMOD flow')
+axs[2][0].set_xlim(-1.2,25)
+axs[2][0].set_ylim(-1.2,25)
+axs[2][0].set_aspect('equal')
+axs[2][0].grid(True, linestyle='--',alpha = 0.5)
+
+axs[2][1].scatter(data=hist_flow[(hist_flow['model']=='HYMOD') & (hist_flow['tag']=='2-4')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][1].set_xlim(-1.2,25)
+axs[2][1].set_ylim(-1.2,25)
+axs[2][1].set_aspect('equal')
+axs[2][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[2][2].scatter(data=hist_flow[(hist_flow['model']=='HYMOD') & (hist_flow['tag']=='4-6')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][2].set_xlim(-1.2,25)
+axs[2][2].set_ylim(-1.2,25)
+axs[2][2].set_aspect('equal')
+axs[2][2].grid(True, linestyle='--',alpha = 0.5)
+
+axs[2][3].scatter(data=hist_flow[(hist_flow['model']=='HYMOD') & (hist_flow['tag']=='6-8')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][3].set_xlim(-1.2,25)
+axs[2][3].set_ylim(-1.2,25)
+axs[2][3].set_aspect('equal')
+axs[2][3].grid(True, linestyle='--',alpha = 0.5)
+
+#LSTM
+axs[3][0].scatter(data=hist_flow[(hist_flow['model']=='LSTM') & (hist_flow['tag']=='0-2')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][0].set_ylabel('Simulated LSTM flow')
+axs[3][0].set_xlabel('Truth')
+axs[3][0].set_xlim(-1.2,25)
+axs[3][0].set_ylim(-1.2,25)
+axs[3][0].set_aspect('equal')
+axs[3][0].grid(True, linestyle='--',alpha = 0.5)
+
+axs[3][1].scatter(data=hist_flow[(hist_flow['model']=='LSTM') & (hist_flow['tag']=='2-4')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][1].set_xlabel('Truth')
+axs[3][1].set_xlim(-1.2,25)
+axs[3][1].set_ylim(-1.2,25)
+axs[3][1].set_aspect('equal')
+axs[3][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[3][2].scatter(data=hist_flow[(hist_flow['model']=='LSTM') & (hist_flow['tag']=='4-6')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][2].set_xlabel('Truth')
+axs[3][2].set_xlim(-1.2,25)
+axs[3][2].set_ylim(-1.2,25)
+axs[3][2].set_aspect('equal')
+axs[3][2].grid(True, linestyle='--',alpha = 0.5)
+
+axs[3][3].scatter(data=hist_flow[(hist_flow['model']=='LSTM') & (hist_flow['tag']=='6-8')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][3].set_xlabel('Truth')
+axs[3][3].set_xlim(-1.2,25)
+axs[3][3].set_ylim(-1.2,25)
+axs[3][3].set_aspect('equal')
+axs[3][3].grid(True, linestyle='--',alpha = 0.5)
+
+plt.tight_layout()
+plt.show()
+fig.savefig(f'output/{id}/historical_everything_qqplot.png', dpi=300)
+
+
+
+
+
+##--Future--##
+fig,axs=plt.subplots(4,4, figsize=(9,8))
+#precip
+axs[0][0].scatter(data=future_precip[future_precip['tag']=='0-2'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][0].set_title('MAP RMSE: 0-2')
+axs[0][0].set_ylabel('Interpolated MAP')
+axs[0][0].set_xlim(-10,250)
+axs[0][0].set_ylim(-10,250)
+axs[0][0].grid(True, linestyle='--',alpha = 0.5)
+
+axs[0][1].scatter(data=future_precip[future_precip['tag']=='2-4'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][1].set_title('MAP RMSE: 2-4')
+axs[0][1].set_xlim(-10,250)
+axs[0][1].set_ylim(-10,250)
+axs[0][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[0][2].scatter(data=future_precip[future_precip['tag']=='4-6'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][2].set_title('MAP RMSE: 4-6')
+axs[0][2].set_xlim(-10,250)
+axs[0][2].set_ylim(-10,250)
+axs[0][2].grid(True, linestyle='--',alpha = 0.5)
+
+
+axs[0][3].scatter(data=future_precip[future_precip['tag']=='6-8'],
+                  x='truth',y='PRECIP',alpha=0.2,color=palette[0])
+axs[0][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[0][3].set_title('MAP RMSE: 6-8')
+axs[0][3].set_aspect('equal')
+axs[0][3].set_xlim(-10,250)
+axs[0][3].set_ylim(-10,250)
+axs[0][3].grid(True, linestyle='--',alpha = 0.5)
+
+
+#hbv recalibrated
+axs[1][0].scatter(data=future_flow[(future_flow['model']=='HBV Re') & (future_flow['tag']=='0-2')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][0].set_ylabel('Simulated HBV-Re flow')
+axs[1][0].set_xlim(-1.2,50)
+axs[1][0].set_ylim(-1.2,50)
+axs[1][0].grid(True, linestyle='--',alpha = 0.5)
+
+
+axs[1][1].scatter(data=future_flow[(future_flow['model']=='HBV Re') & (future_flow['tag']=='2-4')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][1].set_xlim(-1.2,50)
+axs[1][1].set_ylim(-1.2,50)
+axs[1][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[1][2].scatter(data=future_flow[(future_flow['model']=='HBV Re') & (future_flow['tag']=='4-6')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][2].set_xlim(-1.2,50)
+axs[1][2].set_ylim(-1.2,50)
+axs[1][2].grid(True, linestyle='--',alpha = 0.5)
+
+axs[1][3].scatter(data=future_flow[(future_flow['model']=='HBV Re') & (future_flow['tag']=='6-8')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[1])
+axs[1][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[1][3].set_xlim(-1.2,50)
+axs[1][3].set_ylim(-1.2,50)
+axs[1][3].grid(True, linestyle='--',alpha = 0.5)
+
+#HYMOD
+axs[2][0].scatter(data=future_flow[(future_flow['model']=='HYMOD') & (future_flow['tag']=='0-2')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][0].set_ylabel('Simulated HYMOD flow')
+axs[2][0].set_xlim(-1.2,50)
+axs[2][0].set_ylim(-1.2,50)
+axs[2][0].grid(True, linestyle='--',alpha = 0.5)
+
+axs[2][1].scatter(data=future_flow[(future_flow['model']=='HYMOD') & (future_flow['tag']=='2-4')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][1].set_xlim(-1.2,50)
+axs[2][1].set_ylim(-1.2,50)
+axs[2][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[2][2].scatter(data=future_flow[(future_flow['model']=='HYMOD') & (future_flow['tag']=='4-6')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][2].set_xlim(-1.2,50)
+axs[2][2].set_ylim(-1.2,50)
+axs[2][2].grid(True, linestyle='--',alpha = 0.5)
+
+axs[2][3].scatter(data=future_flow[(future_flow['model']=='HYMOD') & (future_flow['tag']=='6-8')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[2])
+axs[2][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[2][3].set_xlim(-1.2,50)
+axs[2][3].set_ylim(-1.2,50)
+axs[2][3].grid(True, linestyle='--',alpha = 0.5)
+
+#LSTM
+axs[3][0].scatter(data=future_flow[(future_flow['model']=='LSTM') & (future_flow['tag']=='0-2')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][0].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][0].set_ylabel('Simulated LSTM flow')
+axs[3][0].set_xlabel('Truth')
+axs[3][0].set_xlim(-1.2,50)
+axs[3][0].set_ylim(-1.2,50)
+axs[3][0].grid(True, linestyle='--',alpha = 0.5)
+
+axs[3][1].scatter(data=future_flow[(future_flow['model']=='LSTM') & (future_flow['tag']=='2-4')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][1].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][1].set_xlabel('Truth')
+axs[3][1].set_xlim(-1.2,50)
+axs[3][1].set_ylim(-1.2,50)
+axs[3][1].grid(True, linestyle='--',alpha = 0.5)
+
+axs[3][2].scatter(data=future_flow[(future_flow['model']=='LSTM') & (future_flow['tag']=='4-6')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][2].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][2].set_xlabel('Truth')
+axs[3][2].set_xlim(-1.2,50)
+axs[3][2].set_ylim(-1.2,50)
+axs[3][2].grid(True, linestyle='--',alpha = 0.5)
+
+axs[3][3].scatter(data=future_flow[(future_flow['model']=='LSTM') & (future_flow['tag']=='6-8')],
+                  x='truth',y='streamflow',alpha=0.2,color=palette[3])
+axs[3][3].axline(xy1=(0,0), slope=1, color='#555555')
+axs[3][3].set_xlabel('Truth')
+axs[3][3].set_xlim(-1.2,50)
+axs[3][3].set_ylim(-1.2,50)
+axs[3][3].grid(True, linestyle='--',alpha = 0.5)
+
+plt.tight_layout()
+plt.show()
+fig.savefig(f'output/{id}/future_everything_qqplot.png', dpi=300)
